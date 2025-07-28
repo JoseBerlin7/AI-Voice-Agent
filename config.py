f399b11640db22555c8fc4c23f1dad42c3a02bce
Jose Berlin <josej510584@gmail.com>
2025-07-28 07:05:18 +0100

    updated tts, voice_id

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ OPENAI_DEPLOYMENT = os.getenv("OPENAI_DEPLOYMENT")
 AZURE_SPEECH_KEY = os.getenv("API_KEY")
 AZURE_REGION = os.getenv("AZURE_REGION")
 
-TTS_VOICE_ID = os.getenv("TTS_VOICE_ID")
+TTS_VOICE_ID = os.getenv("TTS_VOICE_ID", "21m00Tcm4TlvDq8ikWAM")
 STT_ENDPOINT = os.getenv("STT_ENDPOINT")
 
 VAPI_API_KEY = os.getenv("VAPI_API_KEY")
